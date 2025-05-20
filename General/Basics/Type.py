fe9c220871e1b391e6278a4f872af71b6f8d00ca
Falk Liebezeit <falk.liebezeit@outlook.com>
2025-05-20 16:09:12 +0200

    Add files via upload

---
+++ General/Basics/Type.py
@@ -0,0 +1,7 @@
+VAR1 = 15
+VAR2 = "Welcome in Python"
+VAR3 = [12, 23, 43]
+
+print("It is of type Integer",type(VAR1))
+print("It is of type String",type(VAR2))
+print("It is of type List",type(VAR3))
