591f01bdade66914d3d67b379acdfc1e883868bc
Falk Liebezeit <falk.liebezeit@outlook.com>
2025-05-20 19:53:01 +0200

    Add files via upload

--- General/Basics/Age checking program using If-Else and Pass statement.py
+++ General/Basics/Age checking program using If-Else and Pass statement.py
@@ -1,13 +1,12 @@
-# Age checking program: If user entered age > 100 then do nothing, just pass through, else print welcome message
- 
-age = int(input("Enter your age: "))
-# use pass inside if statement
-# if enetered age > 100 then simply pass
-# else print a message for user
+# Age checking program
+# If the entered age is greater than 100, do nothing (use pass)
+# Otherwise, print a welcome message
+
+age = int(input("Enter your age: "))  # Get user input and convert it to an integer
 
 if age > 100:
-    pass
+    pass  # No action is taken when age is greater than 100
 else:
-    print("Your age is : ", num)
-    
-print('Welcome User!!')
+    print("Your age is:", age)  # Print the entered age if it is 100 or less
+
+print('Welcome User!!')  # Always prints the welcome message
