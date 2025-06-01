e8a5e2ff8e68f2f9a31c85cb55571cc6d6435a93
Falk Liebezeit <falk.liebezeit@outlook.com>
2025-06-01 10:00:45 +0200

    Delete General/PYTHON language features directory

--- General/PYTHON language features/Call Function.py
+++
@@ -1,6 +0,0 @@
-def power(x, y):
-    return x ** y
-
-#Call the function:
-a =power(2, 3)
-print(a)
--- General/PYTHON language features/Swap.py
+++
@@ -1,9 +0,0 @@
-X=5
-Y=10
-TEMP=Y
-X=Y
-Y=TEMP
-print ('The value of X after swapping:{}'.format(X))
-print('The value of Y after swapping:{}'.format(Y))
-
-
