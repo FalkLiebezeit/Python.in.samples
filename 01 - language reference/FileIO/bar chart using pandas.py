93565c369ba3a72ac35af1efb943a3e7c674b446
FalkLiebezeit <falk.liebezeit@outlook.com>
2025-12-10 19:35:02 +0100

    101225

--- 01 - language reference/FileIO/bar chart using pandas.py
+++ 01 - language reference/FileIO/bar chart using pandas.py
@@ -1,6 +1,7 @@
 # bar chart using pandas
 import pandas as pd
 import matplotlib.pyplot as plt
+
 df= pd.read_csv('C:/Users/Subbu/Desktop/winesreview.csv')
 df['points'].value_counts().sort_index().plot.bar(title='wines Review')
 plt.xlabel('points')
